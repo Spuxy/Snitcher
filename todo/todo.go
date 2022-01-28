101176a49cd5aca98051fae2a2a71b12403163da
Filip Boye-Kofi <Spuxy@seznam.cz>
2022-01-28 18:51:44 +0100

    🌟 edited todo to replace and crea new file

--- todo/todo.go
+++ todo/todo.go
@@ -22,39 +22,70 @@ func New(d string, f string, l int) Todo {
 }
 
 func GetTodosFromFile(file string) {
-	g := github.Github{TOKEN: "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"}
+	var g github.Github = github.Github{TOKEN: "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"}
+	var lineNumber int
+	var numberLineToScan []int
+	fileFullContent, err := os.ReadFile(file)
 
-	f, err := os.Open(file)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	f, err := os.Open(file)
 	defer f.Close()
+	if err != nil {
+		panic(err.Error())
+	}
 
 	scanner := bufio.NewScanner(f)
-	lineNumber := 0
-	var numberLineToScan []int
+
+	// iteration over scanjner
 	for scanner.Scan() {
 		line := scanner.Text()
+
+		// takes line and check if exists todo comment
 		if ok := findByReg(line); ok {
+
+			// format todo line -> "// TODO" etc..
 			todo := trimFormat(line)
-			t := New(todo, file, lineNumber)
-			fmt.Println(t)
+
 			issue := map[string]interface{}{"title": "issue " + todo, "body": todo}
 			_, err := g.SendReq("POST", "https://api.github.com/repos/Spuxy/snitchify/issues", issue)
 			if err != nil {
 				fmt.Println("Request Error: ", err.Error())
 			}
+
 			numberLineToScan = append(numberLineToScan, lineNumber)
+			fileFullContent = replaceTodo(file, todo, fileFullContent)
 		}
 		lineNumber++
-		fmt.Println("line:", lineNumber, line)
-		fmt.Println(numberLineToScan)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	// create file
+	createdFile, err := os.Create(file)
+	defer createdFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(fileFullContent))
+	createdFile.WriteString(string(fileFullContent))
+
+	// remove file
+	os.Remove(file)
+}
+
+// return whole content file in string
+func replaceTodo(fileName string, todo string, fileFullContentv2 []byte) []byte {
+	// fileFullContent, _ := os.ReadFile(fileName)
+	rg, err := regexp.Compile("^(.*)TODO(O*): " + todo)
+	if err != nil {
+		fmt.Println("error: ", err.Error())
+	}
+	return rg.ReplaceAll(fileFullContentv2, []byte("DONE"))
 }
 
 func findByReg(line string) bool {
